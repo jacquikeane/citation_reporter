bac90472031596aa83a085b99237457f5170c377
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-09-17 10:15:48 +0100

    Fix update_publications so that it works with cron

--- sanger_deployment/update_publications.sh
+++ sanger_deployment/update_publications.sh
@@ -3,10 +3,11 @@ set -ex
 WORKING_DIR="/var/opt/citation_reporter_prod"
 VENV="${WORKING_DIR}/venv"
 MONIT_CONFIG="${WORKING_DIR}/monitrc"
+MONIT_BINARY="${WORKING_DIR}/monit/bin/monit"
 
 . ${VENV}/bin/activate
-monit -c $MONIT_CONFIG stop all
+$MONIT_BINARY -c $MONIT_CONFIG stop all
 ${WORKING_DIR}/citation_reporter_web update-publications
-monit -c $MONIT_CONFIG start citation_reporter_web
+$MONIT_BINARY -c $MONIT_CONFIG start citation_reporter_web
 sleep 60
-monit -c $MONIT_CONFIG start all
+$MONIT_BINARY -c $MONIT_CONFIG start all
